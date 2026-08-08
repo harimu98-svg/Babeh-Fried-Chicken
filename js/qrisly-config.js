3889ae92336b99b82d404f129253131b1a4d8241
harimu98-svg <harimu98@gmail.com>
2026-08-08 12:23:31 +0700

    Create qrisly-config.js

---
+++ js/qrisly-config.js
@@ -0,0 +1,9 @@
+// js/qrisly-config.js
+
+const QRISLY_CONFIG = {
+    
+    // Base URL untuk frontend
+    baseUrl: '/.netlify/functions/qrisly', // Panggil Netlify Function
+};
+
+window.QRISLY_CONFIG = QRISLY_CONFIG;
